647df3e9b66059a2f3aeb0e609bcb6d6d1b4eb7c
Dadidam <joker.pnz@gmail.com>
2017-09-19 14:27:32 -0400

    Add logger, routes and static html file

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const Hapi = require('hapi');
+const Good = require('good');
 
 // Create a server with a host and port
 const server = new Hapi.Server();
@@ -9,15 +10,7 @@ server.connection({
     port: 8000 
 });
 
-// Add routes
-// server.route({
-//     method: 'GET',
-//     path:'/hello', 
-//     handler: function (request, reply) {
-
-//         return reply('hello world');
-//     }
-// });
+// Routing rules
 server.route({
     method: 'GET',
     path: '/',
@@ -34,11 +27,50 @@ server.route({
     }
 });
 
-// Start the server
-server.start((err) => {
-
+// Work with static files
+server.register(require('inert'), (err) => {
+    
     if (err) {
         throw err;
     }
-    console.log('Server running at:', server.info.uri);
+
+    server.route({
+        method: 'GET',
+        path: '/hello',
+        handler: function (request, reply) {
+            reply.file('./public/hello.html'); // <== static HTML page
+        }
+    });
+});
+
+// Register and start the server
+server.register({
+    register: Good,
+    options: {
+        reporters: {
+            console: [{
+                module: 'good-squeeze',
+                name: 'Squeeze',
+                args: [{
+                    response: '*',
+                    log: '*'
+                }]
+            }, {
+                module: 'good-console'
+            }, 'stdout']
+        }
+    }
+}, (err) => {
+
+    if (err) {
+        throw err; // something bad happened loading the plugin
+    }
+
+    server.start((err) => {
+
+        if (err) {
+            throw err;
+        }
+        server.log('info', 'Server running at: ' + server.info.uri);
+    });
 });
