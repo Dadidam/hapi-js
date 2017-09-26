68808d77899aa8b924e5150e1a19b15dac86d5a0
Dadidam <joker.pnz@gmail.com>
2017-09-26 16:33:47 -0400

    Add Blipp plugin to show all available routes

---
+++ hapi-hello.js
@@ -0,0 +1,29 @@
+const Hapi = require('hapi');
+const Blipp = require('blipp');
+const server = new Hapi.Server();
+
+server.connection({
+    port: 1337,
+    host: '127.0.0.1'
+});
+
+server.route({
+    method: 'GET',
+    path: '/',
+    handler: function(request, reply) {
+        return reply('Hello Hapi World\n');
+    }
+});
+
+server.register(Blipp, (err) => {
+    if (err) {
+        throw err;
+    }
+
+    server.start((err) => {
+        if (err) {
+            throw err;
+        }
+        console.log(`Server running at ${server.info.uri}`);
+    });
+});
