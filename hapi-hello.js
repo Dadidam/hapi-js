782b7eeb1c4776c95cb0e5facb1c2f81624946df
Dadidam <joker.pnz@gmail.com>
2017-09-28 14:34:57 -0400

    Add sample routes with different replies

--- hapi-hello.js
+++ hapi-hello.js
@@ -15,6 +15,47 @@ server.route({
     }
 });
 
+// Reply with Error() object
+server.route({
+    method: 'GET',
+    path: '/error',
+    handler: function(request, reply) {
+        return reply(new Error());
+    }
+});
+
+// Other routes for other methods
+server.route({
+    method: '*',
+    path: '/{p*}',
+    handler: function(request, reply) {
+        return reply('The page was not found').code(404).type('text/plain').header('X-Custom', 'value');
+    }
+});
+
+server.route({
+    method: 'GET',
+    path: '/hello/{name}',
+    config: {
+        description: 'Return an object with hello message',
+        validate: {
+            params: {
+                //name: Joi.string().min(3).required()
+            }
+        },
+        pre: [],
+        handler: function (request, reply) {
+            const name = request.params.name;
+            return reply({
+                message: `Hello, ${name}`
+            });
+        },
+        cache: {
+            expiresIn: 3600000
+        }
+    },
+});
+
 server.register(Blipp, (err) => {
     if (err) {
         throw err;
